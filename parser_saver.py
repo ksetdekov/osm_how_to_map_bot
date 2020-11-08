11404f6704d225b6ae7b3ea2e790bd8102787584
ksetdekov <ksetdekov@gmail.com>
2020-11-08 22:48:18 +0300

    done with parser_saver.py

--- parser_saver.py
+++ parser_saver.py
@@ -6,53 +6,7 @@ import bs4
 import requests
 from fuzzywuzzy import fuzz
 
-# from lxml.html.clean import unicode
-
-how_to_map = requests.get('https://wiki.openstreetmap.org/wiki/RU:%D0%9A%D0%B0%D0%BA_%D0%BE%D0%B1%D0%BE%D0%B7%D0%BD'
-                          '%D0%B0%D1%87%D0%B8%D1%82%D1%8C').text
-soup = BeautifulSoup(how_to_map, 'lxml')
-
-h3s = soup.find_all('h3')
-# тут вводится какое поиск мы делаем
-Str2 = "здание"
-matches = {}
-for i in h3s:
-    Str1 = i.text
-    Partial_Ratio = fuzz.ratio(Str1.lower(), Str2.lower())
-    matches[i.text] = (Partial_Ratio, i)
-
-x = sorted(matches, key=lambda k: matches[k][0], reverse=True)
-print(matches[x[0]][0], x[0])  # находим раздел
-
-# todo найти способ выбрать все между h3
-# todo найти способ вывести это
-# todo найти способ вывести это по запросу
-position = h3s.index(matches[x[0]][1])
-
-for header in h3s[position:position+1]:
-    nextNode = header
-    while True:
-        nextNode = nextNode.nextSibling
-        if nextNode is None:
-            break
-        if isinstance(nextNode, bs4.NavigableString):
-            print(nextNode.strip())
-        if isinstance(nextNode, bs4.Tag):
-            if nextNode.name == "h3":
-                break
-            print(nextNode.get_text().strip())
-
-now = datetime.datetime.now()
-with open("parse.html", "w", newline='', encoding="utf-8") as f:
-    f.write(how_to_map)
-
-# read it back in
-with open("parse.html", newline='', encoding="utf-8") as f:
-    soup = BeautifulSoup(f, 'lxml')
-    # do something with soup
-# set the limit how often to update
-wait_time_to_update = datetime.timedelta(minutes=1)
-
+# set db connection
 db = sqlite3.connect("data.sqlite")
 conn = db.cursor()
 
@@ -65,26 +19,65 @@ CREATE TABLE IF NOT EXISTS "updates" (
 
 
 def add_time(status):
+    """write time of parse data update"""
     conn.execute("INSERT INTO updates (stamp,ok) VALUES (?,?)", (datetime.datetime.now(), status))
     db.commit()
 
 
 def get_last_update():
+    """get time of last parse data update"""
     conn.execute("SELECT stamp FROM updates WHERE ok = 1 ORDER BY stamp DESC LIMIT 1")
     return datetime.datetime.strptime(conn.fetchone()[0], '%Y-%m-%d %H:%M:%S.%f')
 
 
-print((datetime.datetime.now() - get_last_update()) > wait_time_to_update)
-
-
 def get_soup(wait=1):
+    """если данные обновлены свежее, чем wait минут назад - взять с диска, иначе - парсить с сайта"""
     now_time = datetime.datetime.now()
     wait_time = datetime.timedelta(minutes=wait)
-    print()
     if (now_time - get_last_update()) > wait_time:
-        print('get new')
+        how_to_map = requests.get(
+            'https://wiki.openstreetmap.org/wiki/RU:%D0%9A%D0%B0%D0%BA_%D0%BE%D0%B1%D0%BE%D0%B7%D0%BD'
+            '%D0%B0%D1%87%D0%B8%D1%82%D1%8C').text  # страница, которую обрабатываем
+        bs_soup = BeautifulSoup(how_to_map, 'lxml')
+        with open("parse.html", "w", newline='', encoding="utf-8") as f:
+            f.write(how_to_map)
+        return bs_soup
     else:
-        print('read from disk')
+        # read it back in
+        with open("parse.html", newline='', encoding="utf-8") as f:
+            bs_soup = BeautifulSoup(f, 'lxml')
+        return bs_soup
 
 
-get_soup()
+def result_find(question):
+    """это функция, которая находит раздел, уровень соответствия и содеражимое по запросу"""
+    soup = get_soup()
+
+    h3s = soup.find_all('h3')
+    # тут вводится какое поиск мы делаем
+    str2 = question
+    matches = {}
+    for i in h3s:
+        str1 = i.text
+        partial_ratio = fuzz.ratio(str1.lower(), str2.lower())
+        matches[i.text] = (partial_ratio, i)
+
+    x = sorted(matches, key=lambda k: matches[k][0], reverse=True)
+    section, confidence = matches[x[0]][0], x[0]  # находим раздел
+
+    position = h3s.index(matches[x[0]][1])
+
+    text_content = []  # basic response
+    for header in h3s[position:position + 1]:
+        next_node = header
+        while True:
+            next_node = next_node.nextSibling
+            if next_node is None:
+                break
+            if isinstance(next_node, bs4.NavigableString):
+                text_content.append(next_node.strip())
+            if isinstance(next_node, bs4.Tag):
+                if next_node.name == "h3":
+                    break
+                text_content.append(next_node.get_text().strip())
+    return confidence, section, ''.join(text_content)
